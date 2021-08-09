a1d9bb92aacf68266b289dc131559603fc6b61c3
Ezequiel Romero Bertani <71280667+ezeqrb@users.noreply.github.com>
2021-08-09 20:32:22 -0300

    transporter

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const transporter = require ('./transporter')
 const express = require('express')
-const app = express()
 const port = 3000
 const nodemailer = require("nodemailer");
-import transporter from './transporter'
 const cors = require('cors')
+const app = express()
 
 app.use(express.json())
 app.use(cors())
